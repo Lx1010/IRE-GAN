1f40e89befcc57a4ae75026f506bc05751ebeee3
yashwanthyuvi <yashwanthregatte@gmail.com>
2017-10-23 18:22:21 +0530

    all files in data folder

--- test.py
+++ test.py
@@ -1,30 +1,41 @@
 import re
 import sys
+import os
 article_id = {}
 id_article = {}
 user_id = {}
 id_user = {}
 u_article = {}
+user_article_map = {}
 data_folder = sys.argv[1]
 def main ():
-	fp = open(data_folder + "01-05-2017")
 	users = []
 	articles = []
-	count = 0
-	for line in fp:
-		count +=1
-		if(line.split()[0]=="an"):
-			temp = line.split("\t")
-			user = temp[1]
-			users.append(temp[1])
-			new = temp[2].split("[")
-			new = new[1].split("]")[0]
-			user_articles = new.split(",")
-			u_article[user] = user_articles
-			# print user_articles
-			articles += user_articles
+	files = os.listdir(data_folder)
+
+	for file in files:
+		fp = open(data_folder + file)
+		for line in fp:
+			if(line.split()[0]=="an"):
+				temp = line.split("\t")
+				user = temp[1]
+				users.append(temp[1])
+				new = temp[2].split("[")
+				new = new[1].split("]")[0]
+				user_articles = new.split(",")
+				for j in range(len(user_articles)):
+					user_articles[j] = user_articles[j].strip('"')
+				if(user not in u_article):
+					u_article[user] = user_articles
+				else:
+					u_article[user] += user_articles
+
+				articles += user_articles
+
+
 	articles = list(set(articles))
 	users = list (set(users))
+	# print len(users) , len(articles)
 	count = 0
 	
 	for i in articles:
@@ -44,10 +55,10 @@ def main ():
 		new_list=[]
 		for j in range(len(u_article[i])):
 			new_list += [article_id[u_article[i][j]]]
-		# del u_article[i]
-		new_dict[temp] = new_list
-	# print new_dict
 
-	# print len(users)
+		new_dict[temp] = new_list
+
+	user_article_map = new_dict
+	
 
 main()
